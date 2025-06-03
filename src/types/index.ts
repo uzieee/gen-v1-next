d4f3ce213014c29176e9b2ae2e1b287c566a0a60
IrakozeLoraine <mukezwa@gmail.com>
2025-06-03 21:17:50 +0200

    ref: enforce string validation before digits

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,10 +8,10 @@ export type ButtonState = 'default' | 'disabled' | 'active';
 export const signInSchema = z.object({
     phoneNumber: z.string({
         required_error: 'Phone number is required',
-    }).length(9, {
-        message: 'Phone number must be 9 digits long',
     }).regex(/^\d+$/, {
         message: 'Phone number must contain only digits',
+    }).length(9, {
+        message: 'Phone number must be 9 digits long',
     })
 });
 
@@ -20,11 +20,11 @@ export type ISignInDetails = z.infer<typeof signInSchema>;
 export const verifySchema = z.object({
     verificationCode: z.string({
         required_error: 'Verification code is required',
-    }).length(6, {
-        message: 'Verification code must be 6 digits long',
     }).regex(/^\d+$/, {
         message: 'Verification code must contain only digits',
-    })
+    }).length(6, {
+        message: 'Verification code must be 6 digits long',
+    }),
 });
 
 export type IVerifyDetails = z.infer<typeof verifySchema>;
