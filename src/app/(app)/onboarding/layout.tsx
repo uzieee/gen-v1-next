93af86a114ed99a258f91e4cbed5beef25a3f15b
IrakozeLoraine <mukezwa@gmail.com>
2025-06-22 17:55:25 +0200

    feat: add edit profile page

--- src/app/(app)/onboarding/layout.tsx
+++
@@ -1,9 +0,0 @@
-import PageTransition from "@/components/motion/PageTransition";
-
-export default function OnboardingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <PageTransition>{children}</PageTransition>;
-}
